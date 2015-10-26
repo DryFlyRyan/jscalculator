cc55def925de235b13b2cf482d36d5d8ce8091a5
Ryan Douglas <ryan@luckydogdigital.com>
2015-10-26 08:55:24 -0600

    updated calc a bit

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 $(document).ready(function () {
-alert("document ready")
+// console.log("document ready");
 var display = '0';
 var input1 = null;
 var input2 = null;
 var operation = '';
+var totaled = false;
 
 $('.display').html(display);
 
-var concatNumber = function (a) {
-  // alert("function!");
+var concatNumberEnd = function (a) {
+  // console.log("function!");
   display = display + a;
+  pushDisplay();
+};
+
+var concatNumberStart = function (a) {
+  // console.log("function!");
+  display = a + display;
   pushDisplay();
 };
 
 var functPress = function (a) {
   input1 = display;
   display = a;
-}
+};
 
 var initialOperation = function (a) {
   input1 = parseInt(display);
-  // alert("idCall = " + a);
-  // alert("input = " + input1);
+  // console.log("idCall = " + a);
+  // console.log("input = " + input1);
   display = a;
   operation = a;
   pushDisplay();
-}
+};
 
 var operationRun = function (a, b){
-  if (operation === '%') {
-    input1 = parseInt(display);
-    input1 = input1 * 100;
-    display = operation + input1.toString();
+  if (operation === '+') {
+    input2 = b.toString();
+    input1, display = a + b;
+    pushDisplay();
+  } else if (operation === '-') {
+    input2 = b.toString();
+    input1, display = a - b;
+    pushDisplay();
+  } else if (operation === '*') {
+    input2 = b.toString();
+    input1, display = a * b;
+    pushDisplay();
+  } else if (operation === '/') {
+    input2 = b.toString();
+    input1, display = a / b;
     pushDisplay();
   }
 }
@@ -45,73 +63,97 @@ var pushDisplay = function () {
 }
 
 $('.number-button').on('click', function () {
+  console.log('num-btn press');
   var idCall = $(this).find('a').attr('id');
   var displayInt = parseInt(display);
-  // alert(displayInt);
-  // alert(displayInt.type() );
+  // console.log(displayInt);
+  // console.log(displayInt.type() );
   var negative = function () {
     displayInt = dispNum * -1;
     display = dispNum.toString();
     pushDisplay();
   }
 
-  if (display === "0") {
-    display = "0";
-    // alert('first if');
+  if (display === "0"
+  // || (!(isNumeric(display)))
+) {
+    console.log('first if');
     display = idCall;
     pushDisplay();
   } else if (idCall === "-/+") {
+    console.log('negative if');
     if (display.indexOf('-') > 0) {
       return;
 
     } else {
-      // alert('negative else');
+      console.log('negative else');
       negative();
     }
   } else if (idCall === ".") {
       if (display.indexOf('.') > 0) {
         return;
       } else {
-        concatNumber(idCall);
+        concatNumberEnd(idCall);
       }
-  } else if (isNaN(displayInt)) {
-      display = idCall;
-      pushDisplay();
-  }
-  else {
-    concatNumber(idCall);
+  } else {
+    console.log('num-button else');
+    concatNumberEnd(idCall);
   }
 });
 
 $('.operation-btn').click(function () {
   var idCall = $(this).find('a').attr('id');
+  // console.log(idCall);
 
-  switch(idCall) {
+  if (input1 === null) {
+    switch(idCall) {
 
-    case 'clear':
-    display = "0";
-    input1 = null;
-    pushDisplay();
-    break;
-    case '=':
-
-    if (input2.length) {
-    input2 = parseInt(display);
-    } else {
+      case 'clear':
+      display = "0";
+      input1 = null;
+      totaled = false;
+      pushDisplay();
+      break;
+      case '=':
       return;
+      break;
+
+      case '%':
+      if (input1 === null) {
+        input1 = parseInt(display);
+        input1 = input1 * 100;
+        display = idCall + input1.toString();
+        pushDisplay();
+      } else {
+        input2 = parseInt(display);
+        input2 = input2 * 100;
+        display = idCall + input2.toString();
+        pushDisplay();
+      }
+      break;
+      default:
+      console.log('default');
+      initialOperation(idCall);
+      break;
+
     }
-    break;
+  } else {
+    switch(idCall) {
 
-    case '%':
-    input1 = parseInt(display);
-    input1 = input1 * 100;
-    display = operation + input1.toString();
-    pushDisplay();
-    default:
-    // alert('default');
-    initialOperation(idCall);
-    break;
+      case 'clear':
+      display = "0";
+      operator = '';
+      input1 = null;
+      input2 = null;
+      totaled = false;
+      pushDisplay();
+      break;
 
+      default:
+      break;
+
+
+    }
   }
 });
 
