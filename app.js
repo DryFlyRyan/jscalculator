a93e57adfe405aa70139380ba766057c960637e5
Ryan Douglas <ryan@luckydogdigital.com>
2015-10-26 20:29:40 -0600

    updates on app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-// console.log("document ready");
+console.log("document ready");
 
 // Holding Variables
 var display = '0';
@@ -27,44 +27,6 @@ var concatStart = function (a) {
   pushDisplay();
 };
 
-// var functPress = function (a) {
-//   input1 = display;
-//   display = a;
-// };
-
-// var initialOperation = function (a) {
-//   input1 = parseInt(display);
-//   // console.log("idCall = " + a);
-//   // console.log("input = " + input1);
-//   display = a;
-//   operation = a;
-//   pushDisplay();
-// };
-
-// var operationRun = function (a, b){
-//   if (operation === '+') {
-//     input2 = b.toString();
-//     input1, display = a + b;
-//     pushDisplay();
-//   } else if (operation === '-') {
-//     input2 = b.toString();
-//     input1, display = a - b;
-//     pushDisplay();
-//   } else if (operation === '*') {
-//     input2 = b.toString();
-//     input1, display = a * b;
-//     pushDisplay();
-//   } else if (operation === '/') {
-//     input2 = b.toString();
-//     input1, display = a / b;
-//     pushDisplay();
-//   }
-// }
-
-// var secondaryOperation = function () {
-//
-// }
-
 var pushDisplay = function () {
   $('.display').html(display);
 }
@@ -73,7 +35,7 @@ var pushDisplay = function () {
 
 //Number Input Handlers
 
-$('.number-button').on('click', function () {
+$('.number-button, .operation-btn').on('click', function () {
   console.log('num-btn press');
   var idCall = $(this).find('a').attr('id');
   var displayInt = parseInt(display);
@@ -90,35 +52,38 @@ $('.number-button').on('click', function () {
     input1 = null;
     totaled = false;
     pushDisplay();
-  } else if (display === "0"
-  // || (!(isNumeric(display)))
-) {
+  } else if (display === "0") {
     console.log('first if');
     display = idCall;
     pushDisplay();
-  } else if (idCall === "-/+") {
+  }
+
+  else if (idCall === "-/+") {
     console.log('negative if');
     if (display.indexOf('-') >= 0
         && display.lastIndexOf('-') !== (display.lastIndexOf('--')+1)
         && display.lastIndexOf('-') > display.lastIndexOf(/[*+\-/()]/)) {
             var target = display.lastIndexOf('-');
-            var split = [display.split(target, 1)].join();
-            display = split;
+            var split = display.split(target, 1);
+            var splitJoin = split.join('');
+            display = splitJoin;
             pushDisplay();
-    }
-
     } else {
       console.log('negative else');
       var target = display.lastIndexOf(/[*+\-/()]/);
-      display = [display.slice(target, 0) + '-' + display.slice(target)].join;
+      var slice = [display.slice(0, target) + '-' + display.slice(target)].join;
     }
-  } else if (idCall === ".") {
+  }
+
+  else if (idCall === ".") {
       if (display.indexOf('.') >= 0 && (display.indexOf(/[*+\-/()]/) < display.indexOf('.'))) {
         return;
       } else {
         concatEnd(idCall);
       }
-  } else {
+  }
+
+  else {
     console.log('num-button else');
     concatEnd(idCall);
   }
@@ -126,61 +91,61 @@ $('.number-button').on('click', function () {
 
 // Operations Input Handlers
 
-$('.operation-btn').click(function () {
-  var idCall = $(this).find('a').attr('id');
+// $('.operation-btn').click(function () {
+//   var idCall = $(this).find('a').attr('id');
   // console.log(idCall);
 
-  if (input1 === null) {
-    switch(idCall) {
-
-      case 'clear':
-      display = "0";
-      input1 = null;
-      totaled = false;
-      pushDisplay();
-      break;
-      case '=':
-      return;
-      break;
-
-      case '%':
-      if (input1 === null) {
-        input1 = parseInt(display);
-        input1 = input1 * 100;
-        display = idCall + input1.toString();
-        pushDisplay();
-      } else {
-        input2 = parseInt(display);
-        input2 = input2 * 100;
-        display = idCall + input2.toString();
-        pushDisplay();
-      }
-      break;
-      default:
-      console.log('default');
-      initialOperation(idCall);
-      break;
-
-    }
-  } else {
-    switch(idCall) {
-
-      case 'clear':
-      display = "0";
-      operator = '';
-      input1 = null;
-      input2 = null;
-      totaled = false;
-      pushDisplay();
-      break;
-
-      default:
-      break;
-
-
-    }
-  }
-});
+//   if (input1 === null) {
+//     switch(idCall) {
+//
+//       case 'clear':
+//       display = "0";
+//       input1 = null;
+//       totaled = false;
+//       pushDisplay();
+//       break;
+//       case '=':
+//       return;
+//       break;
+//
+//       case '%':
+//       if (input1 === null) {
+//         input1 = parseInt(display);
+//         input1 = input1 * 100;
+//         display = idCall + input1.toString();
+//         pushDisplay();
+//       } else {
+//         input2 = parseInt(display);
+//         input2 = input2 * 100;
+//         display = idCall + input2.toString();
+//         pushDisplay();
+//       }
+//       break;
+//       default:
+//       console.log('default');
+//       initialOperation(idCall);
+//       break;
+//
+//     }
+//   } else {
+//     switch(idCall) {
+//
+//       case 'clear':
+//       display = "0";
+//       operator = '';
+//       input1 = null;
+//       input2 = null;
+//       totaled = false;
+//       pushDisplay();
+//       break;
+//
+//       default:
+//       break;
+//
+//
+//     }
+//   }
+// });
 
 
 
